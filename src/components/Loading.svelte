510d17689a8f1cfcc5bb306c4bfc658a12b7c572
HakkinDavid <desr11@hotmail.com>
2025-10-10 05:30:08 -0700

    componente de carga

---
+++ src/components/Loading.svelte
@@ -0,0 +1,13 @@
+<div class="flex items-center justify-center">
+	<svg
+		class="h-10 w-10 animate-spin text-indigo-600"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+	>
+		<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"
+		></circle>
+		<path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+	</svg>
+	<span class="ml-3 font-semibold text-indigo-600">Cargando...</span>
+</div>
